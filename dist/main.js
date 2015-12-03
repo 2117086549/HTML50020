9f8cf6e0c59cb08f0a22211b78f68f263ccc8042
Matt Surabian <matt@mattsurabian.com>
2015-12-02 22:23:03 -0500

    Add footer, update situation typos

--- dist/main.js
+++ dist/main.js
@@ -7,12 +7,12 @@ var SITUATIONS = [
   "Chef eliminated for reasons unrelated to sabotage",
   "Chef forgets key ingredient in the pantry",
   "I've never seen/held this much money before!",
-  "Male chef identified fellow male chef as 'my biggest competition'",
+  "Male chef identifies fellow male chef as their 'biggest competition'",
   "Chef loses all pantry ingredients due to sabotage",
   "Chef tells judge obvious BS story about why they were 'inspired' to use inferior ingredient",
   "I've never made/eaten/heard of that in my life",
   "Tell your tale (Free Space)",
-  "Alton makes up fake backstory for sabotage item",
+  "Alton makes up fake back story for sabotage item",
   "One chef buys every sabotage in single pre-round auction",
   "Someone repeatedly mentions their nationality",
   "Rustic",
@@ -23,7 +23,7 @@ var SITUATIONS = [
   "Chefs must work together or share due to sabotage",
   "$10,000 spent on a single sabotage",
   "Chef straight up makes a different dish from the one requested",
-  "Budding goes higher than a chef's total remaining cash"
+  "Bidding goes higher than a chef's total remaining cash"
 ];
 
 window.onload = function() {
