da43991b2c9c72bb6565b1e41ee0e1774c16d4a8
Matt Surabian <matt@mattsurabian.com>
2015-12-03 19:04:00 -0500

    Add new situations, force middle space to be free

--- dist/main.js
+++ dist/main.js
@@ -11,7 +11,6 @@ var SITUATIONS = [
   "Chef loses all pantry ingredients due to sabotage",
   "Chef tells judge obvious BS story about why they were 'inspired' to use inferior ingredient",
   "I've never made/eaten/heard of that in my life",
-  "Tell your tale (Free Space)",
   "Alton makes up fake back story for sabotage item",
   "One chef buys every sabotage in single pre-round auction",
   "Someone repeatedly mentions their nationality",
@@ -23,7 +22,32 @@ var SITUATIONS = [
   "Chefs must work together or share due to sabotage",
   "$10,000 spent on a single sabotage",
   "Chef straight up makes a different dish from the one requested",
-  "Bidding goes higher than a chef's total remaining cash"
+  "Bidding goes higher than a chef's total remaining cash",
+  "Kiddie Sabotage",
+  "'This sabotage won't go to me' turns out to be wrong",
+  "Sabotage involves headgear",
+  "Alton offers specific advice that chef fails to take",
+  "Three men, one woman",
+  "Bobs participate in sabotage",
+  "Chef accidentally wins auction while attempting to drive price up",
+  "Chef taunts fellow chef with sabotage-related pun",
+  "Chef falls down in or on the way to/from the pantry",
+  "'I can work with this'",
+  "Bleeped curse",
+  "Alton fake-cries",
+  "Single chef given all sabotages in pre-round auction",
+  "Chef too busy cooking to bid",
+  "'Fresh chefs'",
+  "Alton knows how he would do it, but he's not telling",
+  "Alton explains to camera what actually correct thing a chef is doing",
+  "Elevated",
+  "Chef makes own mayonnaise",
+  "Eliminated chef: 'My dish was better/I was the best chef out there'",
+  "Overcooked protein",
+  "Didn't go to culinary school and proud",
+  "Non-restaurant chef made fun of for occupation",
+  "Single raised eyebrow"
+
 ];
 
 window.onload = function() {
@@ -52,9 +76,15 @@ window.onload = function() {
 
       });
 
-      var index = Math.floor(Math.random() * SITUATIONS.length);
-      cell.innerText = SITUATIONS[index];
-      SITUATIONS.splice(index, 1);
+      // Manually assign the middle space as free
+      if (i === 2 && j === 2) {
+        cell.innerText = "Tell your tale (Free Space)";
+      } else {
+        var index = Math.floor(Math.random() * SITUATIONS.length);
+        cell.innerText = SITUATIONS[index];
+        SITUATIONS.splice(index, 1);
+      }
+
     }
   }
 
